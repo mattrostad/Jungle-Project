badd3715b9202412aedb094a53657564d6589cbd
Matt Rostad <matt@rostad.ca>
2023-09-11 19:37:35 -0700

    added Admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,12 @@
 class Admin::DashboardController < ApplicationController
-  
+
   http_basic_authenticate_with name:
   ENV['ADMIN_USERNAME'], 
   password: ENV['ADMIN_PASSWORD']
 
   def show
+    @product_count = Product.count
+
+    @category_list = Product.group(:category_id).count
   end
 end
